6cec64ba7f02da1b7a7fd296c682ca0306c8d18b
Nikita Bychkov <sandcave2017@gmail.com>
2026-09-17 16:37:35 +0500

    feat(mergesort): add buffer to mergesort

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,58 @@
+public class MergeSort {
+
+    public static void mergeSort(int[] dataset){
+        if (dataset == null || dataset.length < 1){
+            return;
+        }
+
+        int[] buffer = new int[dataset.length];
+
+        mergeSort(dataset,buffer,0, dataset.length-1);
+    }
+
+    private static void mergeSort(int[] dataset, int[] buffer, int left, int right){
+//        if (right - left + 1 <= 15){
+//            insertionSort(dataset);
+//        return;
+//        }
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(dataset, buffer, left, mid);
+        mergeSort(dataset, buffer, mid + 1, right);
+
+        merge(dataset, buffer, left, mid, right);
+    }
+
+    private static void merge(int[] dataset, int[] buffer, int left, int mid, int right){
+        for (int i = left; i <= right ; i++) {
+            buffer[i] = dataset[i];
+        }
+
+        int left_buffer = left;
+        int right_buffer = mid + 1;
+        int dataset_index = left;
+
+        while (left_buffer < mid && right_buffer < right + 1){
+            if (buffer[left_buffer] < buffer[right_buffer]){
+                dataset[dataset_index] = buffer[left_buffer];
+                left_buffer++;
+            } else if (buffer[right_buffer] < buffer[left_buffer]) {
+                dataset[dataset_index] = buffer[left_buffer];
+                right_buffer++;
+            }
+            dataset_index++;
+        }
+        while (left_buffer < mid){
+            dataset[dataset_index] = buffer[left_buffer];
+            left_buffer++;
+            dataset_index++;
+        }
+        while (right_buffer < mid){
+            dataset[dataset_index] = buffer[right_buffer];
+            right_buffer++;
+            dataset_index++;
+        }
+    }
+
+}
