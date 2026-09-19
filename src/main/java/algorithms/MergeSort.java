775671f64aa5ad1f622a7eb4838abab8fe8b7bc3
Nikita Bychkov <sandcave2017@gmail.com>
2026-09-19 14:56:26 +0500

    feature(metrics): implement metrics in MergeSort and InsertionSort

--- src/main/java/algorithms/MergeSort.java
+++ src/main/java/algorithms/MergeSort.java
@@ -1,34 +1,38 @@
 package algorithms;
 
+import metrics.Metrics;
+
 import static utils.InsertionSort.insertionSort;
 
 public class MergeSort {
 
-    public static void mergeSort(int[] dataset){
+    public static void mergeSort(int[] dataset, Metrics metrics){
         if (dataset == null || dataset.length <= 1){
             return;
         }
 
+        metrics.startTimer();
         int[] buffer = new int[dataset.length];
 
-        mergeSort(dataset,buffer,0, dataset.length-1);
+        split(dataset,buffer, 0, dataset.length-1, 1, metrics);
+        metrics.stopTimer();
     }
 
-    private static void mergeSort(int[] dataset, int[] buffer, int left, int right){
+    private static void split(int[] dataset, int[] buffer, int left, int right, int currentDepth, Metrics metrics){
         if (right - left + 1 <= 15){
-            insertionSort(dataset, left, right);
+            insertionSort(dataset, left, right, metrics);
         return;
         }
 
         int mid = left + (right - left) / 2;
 
-        mergeSort(dataset, buffer, left, mid);
-        mergeSort(dataset, buffer, mid + 1, right);
+        split(dataset, buffer, left, mid, currentDepth + 1, metrics);
+        split(dataset, buffer, mid + 1, right, currentDepth + 1, metrics);
 
-        merge(dataset, buffer, left, mid, right);
+        merge(dataset, buffer, left, mid, right, metrics);
     }
 
-    private static void merge(int[] dataset, int[] buffer, int left, int mid, int right){
+    private static void merge(int[] dataset, int[] buffer, int left, int mid, int right, Metrics metrics){
         if (right + 1 - left >= 0) System.arraycopy(dataset, left, buffer, left, right + 1 - left);
 
         int left_buffer = left;
@@ -36,6 +40,9 @@ public class MergeSort {
         int dataset_index = left;
 
         while (left_buffer <= mid && right_buffer < right + 1){
+
+            metrics.incrementComparisons();
+
             if (buffer[left_buffer] <= buffer[right_buffer]){
                 dataset[dataset_index] = buffer[left_buffer];
                 left_buffer++;
--- src/main/java/utils/InsertionSort.java
+++ src/main/java/utils/InsertionSort.java
@@ -1,17 +1,26 @@
 package utils;
 
+import metrics.Metrics;
+
 public class InsertionSort {
-    public static void insertionSort(int[] dataset, int left, int right){
-        int buf = 0;
-        int elem = 0;
-        int j = 0;
+    public static void insertionSort(int[] dataset, int left, int right, Metrics metrics){
+
+        int elem;
+        int j;
         for (int i = left + 1; i <= right; i++) {
             elem = dataset[i];
             j = i - 1;
 
-            while (j >= left && dataset[j] > elem){
-                dataset[j + 1] = dataset[j];
-                j -= 1;
+            while (j >= left){
+                metrics.incrementComparisons();
+
+                if (dataset[j] > elem){
+                    dataset[j + 1] = dataset[j];
+                    j--;
+                }
+                else {
+                    break;
+                }
             }
 
             dataset[j + 1] = elem;
