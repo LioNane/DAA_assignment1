c974de2d3e65aca05a053f53301f9312f4a38f56
Nikita Bychkov <sandcave2017@gmail.com>
2026-09-19 10:06:26 +0500

    feature(metrics): implement class Metrics

---
+++ src/main/java/metrics/Metrics.java
@@ -0,0 +1,36 @@
+package metrics;
+
+public class Metrics {
+    private long comparisons = 0;
+    private int maxDepth = 0;
+    private long startTime;
+    private long endTime;
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    public void incrementComparisons(){
+        comparisons++;
+    }
+
+    public void updateDepth(int currentDepth){
+        maxDepth = currentDepth;
+    }
+
+    public void startTimer(){
+        startTime = System.nanoTime();
+    }
+
+    public void stopTimer(){
+        endTime = System.nanoTime();
+    }
+
+    public long getTimeMs(){
+        return (endTime - startTime) / 1000000;
+    }
+}
